e2b307cfbd162c783f250f742e1613e9767e458c
lilkost <111429255+lilkost@users.noreply.github.com>
2024-07-22 13:33:16 +0300

    Add files via upload

--- local/templates/mains/assets/js/components/scrollMobileButtons.js
+++ local/templates/mains/assets/js/components/scrollMobileButtons.js
@@ -5,12 +5,32 @@ const scrollPositionMobileButton = () => {
 
     window.addEventListener('wheel', (e)=> {
         if(e.wheelDeltaY < 0) {
-            console.log('Vniz')
             buttos.style.bottom = '-100px'
         }else {
-            console.log('verh');
             buttos.style.bottom = '0px'
         }
+    });
+
+    let startY, endY;
+
+    document.addEventListener('touchstart', function(event) {
+        startY = event.touches[0].clientY;
+    });
+
+    document.addEventListener('touchmove', function(event) {
+        endY = event.touches[0].clientY;
+        if (startY && endY) {
+            if (endY > startY) {
+                buttos.style.bottom = '0px'
+            } 
+            else {
+                buttos.style.bottom = '-100px'
+            }
+        }
+    });
+
+    document.addEventListener('touchend', function() {
+        startY = endY = 0;
     });
 }
 
