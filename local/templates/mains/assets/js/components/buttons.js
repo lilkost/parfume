416f9a33ae83bce7668c6c831c2218d264a06dfb
lilkost <111429255+lilkost@users.noreply.github.com>
2024-07-26 11:56:24 +0300

    Add files via upload

--- local/templates/mains/assets/js/components/buttons.js
+++ local/templates/mains/assets/js/components/buttons.js
@@ -3,27 +3,30 @@ const buttonsPointer = () => {
     const favoritesBtn = document.querySelectorAll('.favorites-btn');
     const buttonsBusket = document.querySelectorAll('.slide__btn-basket');
 
-    const addClass = (element, className) => {
+    const addClass = (e, element, className) => {
+        e.preventDefault();
+        e.stopPropagation()
         element.classList.add(className)
     }
 
     if(alignButtons) {
         alignButtons.forEach(element => {
-            element.addEventListener('click', ()=> addClass(element, 'is-active'))
+            element.addEventListener('click', (e)=> addClass(e,element, 'is-active'))
         });
     }
 
     if(favoritesBtn) {
         favoritesBtn.forEach(element => {
-            element.addEventListener('click', ()=> {
-                element.addEventListener('click', ()=> addClass(element, 'is-active'))
-            })
+            element.addEventListener('click', (e)=> addClass(e, element, 'is-active'))
         });
     }
 
     if(buttonsBusket) {
         buttonsBusket.forEach(element => {
-            element.addEventListener('click', ()=> {
+            element.addEventListener('click', (e)=> {
+                const parent = element.closest('.slide')
+                e.preventDefault();
+                e.stopPropagation()
                 if(element.classList.contains('is-active')) return;
                 
                 element.classList.add('is-active')
