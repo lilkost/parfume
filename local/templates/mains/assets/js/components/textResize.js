49cf7dd59ef95d5ec64f0b03199e705a774e1673
lilkost <111429255+lilkost@users.noreply.github.com>
2024-09-23 11:28:37 +0300

    Add files via upload

--- local/templates/mains/assets/js/components/textResize.js
+++ local/templates/mains/assets/js/components/textResize.js
@@ -1,23 +1,35 @@
 const textResize = () => {
+
+    const changeResize = (text) => {
+        const textDesk = text.getAttribute('data-text-des');
+        const textMob = text.getAttribute('data-text-mob');
+
+        if(window.innerWidth > 480) {
+            text.innerHTML = textDesk
+        }else {
+            text.innerHTML = textMob;
+        }
+    }
+
     if(document.querySelector('.personal-account__bonus-conditions-accordion-number')) {
         const numbers = document.querySelectorAll('.personal-account__bonus-conditions-accordion-number');
 
-        const changeResize = () => {
-            numbers.forEach(num=> {
-                const textDesk = num.getAttribute('data-text-des');
-                const textMob = num.getAttribute('data-text-mob');
-
-                if(window.innerWidth > 480) {
-                    num.innerHTML = textDesk
-                }else {
-                    num.innerHTML = textMob;
-                }
-            })
+        const forItems = () => {
+            numbers.forEach(itm => {
+                changeResize(itm)
+            });
         }
+        forItems();
 
-        changeResize();
+        window.addEventListener("resize", ()=> forItems());
+    }
 
-        window.addEventListener("resize", ()=> changeResize());
+    if(document.querySelector('.basket-order__aside-button')) {
+        const button = document.querySelector('.basket-order__aside-button');
+
+        changeResize(button)
+
+        window.addEventListener('resize', ()=> changeResize(button));
     }
 }
 
